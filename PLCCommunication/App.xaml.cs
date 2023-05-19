26fb074e367d70e87abb945db2a96a15bbe37f45
namlp1908 <namlp1908@gmail.com>
2023-05-19 22:06:17 +0700

    Update PLCCommunication

--- PLCCommunication/App.xaml.cs
+++ PLCCommunication/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PLCCommunication.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,20 @@ namespace PLCCommunication
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+        }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainview = new MainWindow();
+            PLCViewModel plcViewModel = new PLCViewModel(mainview.Dispatcher);
+
+            MainViewModel mainViewModel = new MainViewModel(mainview.Dispatcher, mainview, plcViewModel);
+            mainview.DataContext = mainViewModel;
+
+            mainview.Show();
+        }
     }
 }
--- PLCCommunication/MainWindow.xaml.cs
+++ PLCCommunication/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PLCCommunication.Commands;
+﻿using NTech.Base.Resources.NNetSocket;
+using PLCCommunication.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,61 +23,10 @@ namespace PLCCommunication
     /// </summary>
     public partial class MainWindow : Window
     {
-        PLC plc = new PLC();
-        Thread threadPlc;
-        public log4net.ILog Logger { get; } = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public MainWindow()
         {
             InitializeComponent();
 
-            plc.CheckLineWithIPPC();
-            plc.LoadPLCXml();
-            plc.InitServer();
-            plc.ResetCmd = new ResetCmd(plc);
-
-            this.DataContext = plc;
-
-            if(plc.ConnectPLC())
-            {
-                Logger.Info("Connected PLC, start thread Plc");
-                threadPlc = new Thread(new ThreadStart(ReadValues));
-                threadPlc.IsBackground = false;
-                threadPlc.Start();
-            }
-        }
-        private void btnStop_Click(object sender, RoutedEventArgs e)
-        {
-            if(threadPlc.IsAlive)
-                threadPlc.Abort();
-        }
-        void ReadValues()
-        {
-            while(true)
-            {
-                plc.Count1 = (uint)plc.PLCInstance.Read(plc.DicRegister["1"]);
-                plc.Count2 = (uint)plc.PLCInstance.Read(plc.DicRegister["2"]);
-                plc.Count3 = (uint)plc.PLCInstance.Read(plc.DicRegister["3"]);
-                plc.Count4 = (uint)plc.PLCInstance.Read(plc.DicRegister["4"]);
-                plc.Count5 = (uint)plc.PLCInstance.Read(plc.DicRegister["5"]);
-                plc.Count6 = (uint)plc.PLCInstance.Read(plc.DicRegister["6"]);
-
-                //Send to data for station
-                string data = plc.Count1 + "," + plc.Count2 + "," + plc.Count3 
-                              + "," + plc.Count4 + "," + plc.Count5 + "," + plc.Count6;
-                plc.NServerSocket.SendMsg(data);
-
-                Dispatcher.Invoke(new Action(() =>
-                {
-                    lbCountNumberPLC1.Text = plc.Count1.ToString();
-                    lbCountNumberPLC2.Text = plc.Count2.ToString();
-                    lbCountNumberPLC3.Text = plc.Count3.ToString();
-                    lbCountNumberPLC4.Text = plc.Count4.ToString();
-                    lbCountNumberPLC5.Text = plc.Count5.ToString();
-                    lbCountNumberPLC6.Text = plc.Count6.ToString();
-                }));
-
-                Thread.Sleep(200);
-            }
         }
     }
 }
